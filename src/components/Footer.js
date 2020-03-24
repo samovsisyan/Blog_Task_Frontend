b6350a9cb74019d33fd62ea8e80d8540e5a32aa7
samovsisyan <samovsisyan9@gmail.com>
2020-03-24 16:50:25 +0400

    Translate Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,60 @@
 import React, {Component} from 'react';
 
+import {withTranslation} from 'react-i18next';
+import Select from 'react-select';
+import { options } from '../config/options';
 
 class Footer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lang: options[0],
+        };
+    }
+
+    changeLang = (lang) => {
+        const {i18n} = this.props;
+        const {value} = lang;
+        this.setState({lang});
+        i18n.changeLanguage(value);
+    };
+
+
     render() {
+        const {lang} = this.state;
+
+        const {t} = this.props;
+        console.log("this.props", this.props);
+
+        console.log("this.state", this.state)
+
+
         return (
             <div>
+                <div className="App-Root">
+
+                    <Select
+                        defaultValue={options[0]}
+                        options={options}
+                        value={lang}
+                        onChange={this.changeLang}
+                        className="App-Select"
+                    />
+                    <h3 className="text-center pt-5">
+                        {t('Home')}
+                    </h3>
+                </div>
+
+
                 <div className="project_blog">
                     <div className="footer_background">
                         <div className="site-section subscribe bg-light">
                             <div className="container">
                                 <form action="#" className="row align-items-center">
                                     <div className="col-md-5 mr-auto">
-                                        <h2>Newsletter Subcribe</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis aspernatur ut at quae omnis
-                                            pariatur obcaecati possimus nisi ea iste!</p>
+                                        <h2>{t('Newsletter Subcribe')}</h2>
+                                        <p>{t('Lorem ipsum dolor sit...')}</p>
                                     </div>
                                     <div className="col-md-6 ml-auto">
                                         <div className="d-flex">
@@ -35,4 +76,4 @@ class Footer extends Component {
     }
 }
 
-export default Footer;
+export default withTranslation ()(Footer);
--- src/locale/en.json.js
+++ src/locale/en.json.js
@@ -7,6 +7,10 @@ const en =  {
         "Sign Up" :"Sign Up",
         "Sign In" :"Sign In",
         "Log Out" :"Sign Out",
+        "Newsletter Subcribe":"Newsletter Subcribe",
+        "Lorem ipsum dolor sit...": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis aspernatur ut at quae omnis pariatur obcaecati possimus nisi ea iste!",
+
+
     }
 };
 
